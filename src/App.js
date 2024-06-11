7bf81054170d0108f42cfa26afa5b16be80c8160
Muhammad Moiz <muhammadm.contractor@atlashxm.com>
2024-06-11 16:06:29 +0500

    create UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./Component/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TDD Companion Form</h1>
+        <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
