05c19f0946d29663cfce455c166b1b1f8bfc7c59
Ehsan <ehsans.contractor@elementsgs.com>
2024-06-20 13:54:38 +0500

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const API_KEY = "AIzaSyCyWmVYjexmNtS2dTGu-8NgRLQaRQLefg4";
+const API_KEY = "";
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
 
