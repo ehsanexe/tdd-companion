ec866635a6b0b76b9d0d7a5b83222be1e7a90ae2
Ehsan <ehsans.contractor@elementsgs.com>
2024-06-24 10:12:24 +0500

    update src/Component/Form/index.jsx

--- src/Component/Form/index.jsx
+++ src/Component/Form/index.jsx
@@ -26,25 +26,23 @@ const Form = ({ isDrawer, setIsDrawer }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const downloadFile = async (fileContent, fileName) => {
-    const blob = new Blob([fileContent], { type: 'text/plain' });
+    const blob = new Blob([fileContent], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = fileName;  // Replace 'filename.txt' with your desired file name
+    link.download = fileName; // Replace 'filename.txt' with your desired file name
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    URL.revokeObjectURL(url);  // Clean up the URL object
+    URL.revokeObjectURL(url); // Clean up the URL object
   };
-
-
 
   const onSubmit = async (data) => {
     setIsLoading(true);
 
     const res = await getGeneratedResponse(data);
-    await downloadFile(res.code, 'code.txt');
-    await downloadFile(res.testCases, 'testCase.txt');
+    await downloadFile(res.code, "code.txt");
+    await downloadFile(res.testCases, "testCase.txt");
 
     setOutput(res);
     setIsLoading(false);
@@ -61,7 +59,7 @@ const Form = ({ isDrawer, setIsDrawer }) => {
       setValue("language", parseStore.language);
       setValue("library", parseStore.library);
       setValue("tags", parseStore.tags);
-      setTags(parseStore.tags);
+      setTags(parseStore.tags ?? []);
     }
   }, []);
 
@@ -72,10 +70,16 @@ const Form = ({ isDrawer, setIsDrawer }) => {
       !values?.library?.length &&
       !values?.tags?.length;
 
-    if (isSettingsEmpty && !store) {
+    const parseStore = JSON.parse(store);
+    const isStoreEmpty =
+      !parseStore?.framework &&
+      !parseStore?.language &&
+      !parseStore?.library?.length &&
+      !parseStore?.tags?.length;
+
+    if (isSettingsEmpty && isStoreEmpty) {
       setIsDrawer(true);
     } else {
-      console.log({ values });
       localStorage.setItem("store", JSON.stringify(values));
     }
   }, [values]);
@@ -87,33 +91,39 @@ const Form = ({ isDrawer, setIsDrawer }) => {
         <Drawer open={isDrawer} onClose={() => setIsDrawer(false)}>
           <div className="drawer">
             <div>
-              <Autocomplete
-                disablePortal
-                id="framework"
-                options={frameworks}
-                sx={{ width: 300 }}
-                value={getValues("framework")}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    {...register("framework", { required: true })}
-                    label="Framework"
+              <Controller
+                name="framework"
+                control={control}
+                render={({ field }) => (
+                  <Autocomplete
+                    {...field}
+                    disablePortal
+                    id="framework"
+                    options={frameworks}
+                    sx={{ width: 300 }}
+                    onChange={(event, newValue) => field.onChange(newValue)}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Framework" />
+                    )}
                   />
                 )}
               />
             </div>
             <div>
-              <Autocomplete
-                disablePortal
-                id="language"
-                options={languages}
-                sx={{ width: 300 }}
-                value={getValues("language")}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    {...register("language", { required: true })}
-                    label="Language"
+              <Controller
+                name="language"
+                control={control}
+                render={({ field }) => (
+                  <Autocomplete
+                    {...field}
+                    disablePortal
+                    id="language"
+                    options={languages}
+                    sx={{ width: 300 }}
+                    onChange={(event, newValue) => field.onChange(newValue)}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Language" />
+                    )}
                   />
                 )}
               />
